d846c91ec81ffc2225bb65928d2a33217b521702
Konstantin Malanchev <hombit@gmail.com>
2017-02-24 10:35:52 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "playground.hpp"
+
+int main() {
+    auto return_code = playground();
+
+    return return_code;
+}
---
+++ playground.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Konstantin Malanchev on 16/02/2017.
+//
+
+#ifndef PLAYGROUND_HPP
+#define PLAYGROUND_HPP
+
+int playground();
+
+#endif //PLAYGROUND_HPP
